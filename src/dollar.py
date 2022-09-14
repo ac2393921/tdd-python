0300a6028df302841de8afdc351fd279f74342ed
ac2393921 <ac2393921@gmail.com>
2022-09-15 01:54:20 +0900

    update: test moduel

--- src/dollar.py
+++ src/dollar.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
+
 from pydantic import BaseModel
 
 
 class Dollar(BaseModel):
     amount: int
 
-    def times(self, multiplier: int):
-        self.amount *= multiplier
+    def times(self, multiplier: int) -> Dollar:
+        return Dollar(amount=self.amount * multiplier)
--- tests/money_test.py
+++ tests/money_test.py
@@ -3,5 +3,9 @@ from src.dollar import Dollar
 
 def test_multiplication():
     five: Dollar = Dollar(amount=5)
-    five.times(2)
-    assert 10 == five.amount
+    product: Dollar = five.times(2)
+    assert 10 == product.amount
+
+    product: Dollar = five.times(3)
+    five.times(3)
+    assert 15 == product.amount
